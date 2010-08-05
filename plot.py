3c04490ffa97627062e2dd04138fb3855b991356
Julius Adorf <jeadorf@gmail.com>
2010-08-05 14:41:34 +0200

    implemented first draft of 1d heat PDE

--- plot.py
+++ plot.py
@@ -5,6 +5,9 @@ import cairo
 import gtk
 import sol
 import math
+import copy
+import time
+import threading
 
 class PlotCanvas(gtk.DrawingArea):
     def __init__(self):
@@ -18,6 +21,7 @@ class PlotCanvas(gtk.DrawingArea):
         self.redraw(ctx)
         return False
     def redraw(self, ctx):
+        ctx = self.window.cairo_create()
         rect = self.get_allocation()
         plot_1d(self.t, ctx, rect.x, rect.y, rect.width, rect.height)  
 
@@ -39,15 +43,21 @@ def plot_1d(t, ctx, x, y, w, h):
     ctx.restore()
 
 def main():
-    pdf = cairo.PDFSurface("waermeleitung.pdf", 600, 100)
-    ctx = cairo.Context(pdf)
     ts, te, n = 5, -5, 300
     t = sol.solve_stationary_1d(ts, te, n)
+    gen_pdf_1d(t)
+    # show_win_1d_stationary(t)
+    show_win_1d()
+
+def gen_pdf_1d(t):
+    pdf = cairo.PDFSurface("waermeleitung.pdf", 600, 100)
+    ctx = cairo.Context(pdf)
     plot_1d(t, ctx, 10, 10, 580, 80)
     pdf.flush()
 
+def show_win_1d_stationary(t):
     win = gtk.Window()
-    win.set_title("Temperature curve, te=%.2f, ts=%.2f, n=%d" % (ts, te, n))
+    win.set_title("Temperature curve, n=%d" % (len(t)-2))
     win.set_default_size(800, 100)
     plot = PlotCanvas()
     plot.t = t
@@ -55,6 +65,43 @@ def main():
     win.connect("destroy", gtk.main_quit)
     win.show_all()
     gtk.main()
+
+import sys, gobject
+
+def show_win_1d():
+    win = gtk.Window()
+    win.set_default_size(800, 100)
+    plot = PlotCanvas()
+    win.add(plot)
+    win.connect("destroy", gtk.main_quit)
+    win.show_all()
+
+    def update(t):
+        plot.t = t
+        plot.redraw(None)
+
+    br = False
+    class SimThread(threading.Thread):
+        def run(self):
+            ts = 5
+            te = 0
+            t_init = [0 for i in xrange(0, 10)]
+            i = 0
+            tc = []
+            for t in sol.sim_heateq_1d(ts, te, t_init, 1500, 5, 0.001):
+                if i % 3 == 0:
+                    gobject.idle_add(update, t)
+                    time.sleep(1./30)
+                i += 1
+                if br:
+                    break
+
+    gtk.gdk.threads_init()
+    simthread = SimThread()
+    simthread.start()
+
+    gtk.main()
+    br = True
 
 if __name__ == "__main__":
     main()
--- sol.py
+++ sol.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import tridiag
+import copy
 
 def solve_stationary_1d(ts, te, n):
     """Solves the stationary heat equation in one dimension.  The Dirichlet
@@ -9,6 +10,10 @@ def solve_stationary_1d(ts, te, n):
     temperature values at grid points 0 and n+1.  The number of interior grid
     points is n > 1. Returns an array with n+2 elements, including the values
     at the boundaries."""
+    return [ 1. * ts + 1. * i * (te - ts) / (n + 1) for i in xrange(0, n+2)]
+
+def solve_stationary_1d_tridiag(ts, te, n):
+    """Same as solve_stationary_1d, just using a tridiagonal system solver."""
     assert n > 1
     d1 = tridiag.Diag(1)
     d2 = tridiag.Diag(-2)
@@ -25,3 +30,33 @@ def solve_stationary_1d(ts, te, n):
     t = tridiag.solve(d1, d2, d3, b, n)
     return [ts] + t + [te]
 
+def sim_heateq_1d(ts, te, t_init, thermal_diffusivity=1, xstep=30, timestep=0.1):
+    n = len(t_init)
+    t = copy.copy(t_init)
+    xstepsq = 1. * xstep * xstep
+    while True:
+        yield [ts] + t + [te] 
+        # Compute derivative at all interior grid points
+        dt = [ 0 for i in xrange(0, n) ]
+        dt[0] = thermal_diffusivity * (ts - 2*t[0] + t[1]) / xstepsq
+        for i in xrange(1, n-1):
+            dt[i] = thermal_diffusivity * (t[i-1] - 2*t[i] + t[i+1]) / xstepsq
+        dt[n-1] = thermal_diffusivity * (t[n-2] - 2*t[n-1] + te) / xstepsq
+        # Euler step
+        for i in xrange(0, n):
+            t[i] += timestep * dt[i]
+
+def gen():
+    while True:
+        yield 1
+
+if __name__ == "__main__":
+    n = 10
+    sim = sim_heateq_1d(-5, 0, [0 for i in xrange(0, n)], 10)
+    i = 0
+    for t in sim:
+        print t
+        i += 1
+        if i > 5000:
+            break
+    print t
