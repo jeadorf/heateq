e689d037190e6d1d22db8be772764a4d5b540659
Julius Adorf <jeadorf@gmail.com>
2010-08-07 03:03:37 +0200

    reverted to faster version

--- heateq/heateqlapl.c
+++ heateq/heateqlapl.c
@@ -7,7 +7,7 @@ PyObject *heateqlapl_apply(PyObject *self, PyObject *args) {
     const PyObject *ttoparr, *tbottomarr, *tleftarr, *trightarr;
     double *t, *txx, *ttop, *tbottom, *tleft, *tright;
     npy_intp m, n;
-    int i, j, k;
+    int i, j;
 
     if (!PyArg_ParseTuple(args, "OOOOOO", &tarr, &txxarr, &ttoparr, &tbottomarr, &tleftarr, &trightarr)) {
         return NULL;
@@ -15,7 +15,6 @@ PyObject *heateqlapl_apply(PyObject *self, PyObject *args) {
 
     m = PyArray_DIM(tarr, 0);
     n = PyArray_DIM(tarr, 1);
-    i = m*n;
 
     t = PyArray_DATA(tarr);
     txx = PyArray_DATA(txxarr);
@@ -25,22 +24,22 @@ PyObject *heateqlapl_apply(PyObject *self, PyObject *args) {
     tleft = PyArray_DATA(tleftarr);
 
     txx[0] = ttop[0] + t[n] + tleft[0] + t[1] - 4 * t[0];
-    for (k = 1; k < n - 1; k++) {
-        txx[k] = ttop[k] + t[k+n] + t[k-1] + t[k+1] - 4*t[k];
+    for (j = 1; j < n - 1; j++) {
+        txx[j] = ttop[j] + t[n+j] + t[j-1] + t[j+1] - 4*t[j];
     }
-    txx[k] = ttop[k] + t[k+n] + t[k-1] + tright[0] - 4*t[k];
-    for (k = n; k < i - n; k++) {
-        txx[k] = t[k-n] + t[k+n] + tleft[k/n-1] + t[k+1] - 4*t[k];
-        for (k += 1, j = k + n - 2; k < j; k++) {
-           txx[k] = t[k-n] + t[k+n] + t[k-1] + t[k+1] - 4*t[k];
+    txx[n-1] = ttop[n-1] + t[2*n-1] + t[n-2] + tright[0] - 4*t[n-1];
+    for (i = 1; i < m - 1; i++) {
+        txx[i*n] = t[(i-1)*n] + t[(i+1)*n] + tleft[i] + t[i*n+1] - 4*t[i*n];
+        for (j = 1; j < n - 1; j++) {
+           txx[i*n+j] = t[(i-1)*n+j] + t[(i+1)*n+j] + t[i*n+j-1] + t[i*n+j+1] - 4*t[i*n+j];
         }
-        txx[k] = t[k-n] + t[k+n] + t[k-1] + tright[k/n-1] - 4*t[k];
+        txx[(i+1)*n-1] = t[i*n-1] + t[(i+2)*n-1] + t[(i+1)*n-2] + tright[i] - 4*t[(i+1)*n-1];
     }
-    txx[k] = t[k-n] + tbottom[0] + tleft[0] + t[k+1] - 4*t[k];
-    for (k += 1; k < i; k++) {
-        txx[k] = t[k-n] + tbottom[k%n] + t[k-1] + t[k+1]- 4*t[k];
+    txx[(m-1)*n] = t[(m-2)*n] + tbottom[0] + tleft[0] + t[(m-1)*n+1] - 4*t[(m-1)*n];
+    for (j = 1; j < n - 1; j++) {
+        txx[(m-1)*n+j] = t[(m-2)*n+j] + tbottom[j] + t[(m-1)*n+j-1] + t[(m-1)*n+j+1]- 4*t[(m-1)*n+j];
     }
-    txx[k] = t[k-n] + tbottom[n-1] + t[k-1] + tright[m-1]- 4*t[k];
+    txx[m*n-1] = t[(m-1)*n-1] + tbottom[n-1] + t[m*n-2] + tright[m-1]- 4*t[m*n-1];
 
     return PyInt_FromLong(0);
 }
