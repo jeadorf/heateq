c59bb0f2cc5728e24916bf23bfede973d1cdfcae
Julius Adorf <jeadorf@gmail.com>
2011-07-28 13:23:35 +0200

    add console script entry point

---
+++ heateq/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import heateq.cli
+
+def main():
+    heateq.cli.main()
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(name = 'Heateq',
       version = '0.1',
       description = 'Heat equation simulator',
       include_dirs=[numpy.get_include(), '/usr/include/cairo'],
+      entry_points = {
+          'console_scripts': [
+              'heateq = heateq.__main__:main',
+          ]},
       ext_modules = [m1, m2],
       packages = setuptools.find_packages(),
       platforms = 'any',
